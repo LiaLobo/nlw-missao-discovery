01e119757be6c63e44c44048ba4ce73373ef948e
LiaLobo <lia.lobo@gmail.com>
2021-06-28 21:09:41 -0300

    fix readme place and quote pattern

--- src/route.js
+++ src/route.js
@@ -6,7 +6,7 @@ const route = express.Router()
 // No segndo parâmetro tem uma função que já tem por padrão dois parâmetros: a request e a response.
 // Então aqui vai ser definido o que vai ser a resposta do servidor quando for feita uma requisição.
 // Nesse caso a respota será renderizar o arquivo index, que estará com o conteúdo da home nele.
-route.get('/', (req, res) => res.render("index"))
+route.get('/', (req, res) => res.render('index'))
 
 // Exportando o arquivo route
 module.exports = route
