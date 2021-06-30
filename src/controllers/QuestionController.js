3ea6b4d88c55c61e3cb98a9307cef8fbb98b8b22
LiaLobo <lia.lobo@gmail.com>
2021-06-30 10:12:09 -0300

    add function to insert values into questions table

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,3 +1,5 @@
+const Database = require('../db/config')
+
 module.exports = {
   index(req, res) {
     const roomId = req.params.roomId
@@ -6,5 +8,24 @@ module.exports = {
     const password = req.body.password
 
     console.log(`password = ${password}`)
+  },
+
+  async create(req, res) {
+    const db = await Database()
+
+    const question = req.body.question
+    const roomId = req.params.roomId
+
+    await db.run(`INSERT INTO questions (
+      question,
+      room,
+      readed
+    ) VALUES (
+      "${question}",
+      ${roomId},
+      0
+    )`)
+
+    res.redirect(`/room/${roomId}`)
   }
 }
